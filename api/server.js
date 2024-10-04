f43f07bd632e993664ae984cb82ef31d6429b07c
SARTHAK GUSAIN <srthkgusain@gmail.com>
2024-10-04 08:56:29 +0530

    Moved backend to Vercel API route

--- api/server.js
+++ api/server.js
@@ -5,7 +5,6 @@ const path = require('path');
 const cors = require('cors');
 
 const app = express();
-const port = 3000;
 
 app.use(cors());
 app.use(express.json());
@@ -113,6 +112,4 @@ app.get('/', (req, res) => {
     res.sendFile(path.join(__dirname, 'public', 'index.html'));
 });
 
-app.listen(port, () => {
-    console.log(`Server running at http://localhost:${port}`);
-});
+module.exports = app;
